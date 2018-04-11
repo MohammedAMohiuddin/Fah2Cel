9bb7b95bcb535158ce38bab8cc8b01979807ca42
mohiuddin <mohiuddin@10.0.0.109>
2018-04-10 20:51:20 -0700

    First Commit to gitHub

---
+++ 7 Fahrenheit to Celsius/src/Fah2Cel.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Fah2Cel {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a Fahreneit value that you want to convert into Celsius: ");
+
+		Scanner sc = new Scanner(System.in);
+		double Fahrenheit = sc.nextDouble();
+		double Celsius = ((Fahrenheit - 32) / 1.8);
+		System.out.println("Celsius: " + Celsius);
+
+	}
+
+}
